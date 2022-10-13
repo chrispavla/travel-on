f16de353bfeed916fbc0bfda9292d060c1c8b2cd
Kristina <kristina.hodges7@gmail.com>
2022-10-13 17:20:39 -0400

    add a new place functionalite, post request

--- app/controllers/point_of_interests_controller.rb
+++ app/controllers/point_of_interests_controller.rb
@@ -1,2 +1,16 @@
 class PointOfInterestsController < ApplicationController
+  wrap_parameters format: []
+  
+  def create 
+    pointOfInterest = PointOfInterest.create!(pointOfInterest_params)
+    render json: pointOfInterest, status: :created
+  end
+
+  private 
+
+  def pointOfInterest_params
+    params.permit(:name, :image, :note, :user_id, :location_id, :category)
+  end
+
+
 end
---
+++ client/src/components/NewPointInterestForm.js
@@ -0,0 +1,105 @@
+import { useState, useContext } from "react";
+import { UserContext } from "../Context/UserProvider";
+
+function NewPointInterestForm({
+  displayedLocation,
+  setNewPlace,
+  setShowNewPointInterestForm,
+}) {
+  let [user, setUser] = useContext(UserContext);
+  const [name, setName] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [note, setNote] = useState("");
+  const [category, setCategory] = useState("");
+  const [error, setError] = useState();
+
+  function handleSubmitNewPointOfInterest(e) {
+    e.preventDefault();
+
+    let newPointOfInterest = {
+      name: name,
+      image: photo,
+      note: note,
+      category: category,
+      user_id: user.id,
+      location_id: displayedLocation.id,
+    };
+
+    fetch("/point_of_interests", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPointOfInterest),
+    }).then((res) => {
+      if (res.ok) {
+        res.json().then((data) => {
+          setNewPlace(data);
+        });
+      } else {
+        res.json().then((error) => setError(error.errors));
+      }
+
+      setShowNewPointInterestForm(false);
+    });
+
+    setName("");
+    setPhoto("");
+    setNote("");
+    setCategory("");
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmitNewPointOfInterest}>
+        <div>
+          <label>
+            Name
+            <input
+              type="text"
+              placeholder="Royal Plaza Hotel"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            ></input>
+          </label>
+        </div>
+        <div>
+          <label>
+            Photo
+            <input
+              type="text"
+              placeholder="Paste you photo link"
+              value={photo}
+              onChange={(e) => setPhoto(e.target.value)}
+            ></input>
+          </label>
+        </div>
+        <div>
+          <label>
+            Note
+            <input
+              type="text"
+              placeholder="Amazing hotel with the best view"
+              value={note}
+              onChange={(e) => setNote(e.target.value)}
+            ></input>
+          </label>
+        </div>
+        <div>
+          <label>
+            Category
+            <select onChange={(e) => setCategory(e.target.value)}>
+              <option></option>
+              <option>Food</option>
+              <option>Hotel</option>
+              <option>Cultural attraction</option>
+            </select>
+          </label>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
+
+export default NewPointInterestForm;

3262c954d1fa710f793324d840a1296f6e2cdbbc
Kristina <kristina.hodges7@gmail.com>
2022-10-19 18:25:45 -0400

    create route for locations

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -9,4 +9,15 @@ class LocationsController < ApplicationController
     render json: location, status: :ok
   end
 
+  def create 
+    location = Location.create!(location_params)
+    render json: location, status: :created
+  end
+
+  private 
+
+  def location_params 
+    params.permit(:city, :country, :latitude, :longitude)
+  end
+
 end
